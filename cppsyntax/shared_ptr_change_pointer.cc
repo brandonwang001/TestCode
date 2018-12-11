344f5937e77ac4e0e6383ed4c4ac47db3672b9e9
wanglinqiang <wanglinqiang@BYTEDANCE.COM>
2018-12-11 15:54:30 +0800

    add

--- cppsyntax/shared_ptr_change_pointer.cc
+++ cppsyntax/shared_ptr_change_pointer.cc
@@ -14,6 +14,11 @@ int main() {
   std::cout << "before a = nullptr" << std::endl;
   a = nullptr;
   std::cout << "after a = nullptr" << std::endl;
+  if (a) {
+    std::cout << "a is normal" << std::endl;
+  } else {
+    std::cout << "a is no pointer" << std::endl;
+  }
   a = std::make_shared<A>(); 
   return 0;
 }
